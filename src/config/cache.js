05118feaf054ec21317c0cc4a7177ff8c8cc0de4
jgabriel1 <josegabriel@det.ufc.br>
2020-09-20 18:34:32 -0300

    Added redis cache configurations.

---
+++ src/config/cache.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PORT: 6379,
+  DB: 0,
+  TTL: 30,
+}
--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,6 @@
+const cache = require('./cache')
+
 module.exports = {
   BASE_URL: 'https://query1.finance.yahoo.com/',
+  cache,
 }
