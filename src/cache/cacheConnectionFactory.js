aad8affe1864fd404e9c00f3a6a8b9d448def4c6
jgabriel1 <josegabriel@det.ufc.br>
2020-09-21 22:43:03 -0300

    It now works even when the cache is not available.

--- src/cache/cacheConnectionFactory.js
+++ src/cache/cacheConnectionFactory.js
@@ -1,21 +1,43 @@
 const cacheManager = require('cache-manager')
 const redisStore = require('cache-manager-redis')
+// const memoryStore = require('cache-manager-memory-store')
+
+const redis = require('redis')
 
 const config = require('../config')
 
 function cacheConnectionFactory() {
-  const cache = cacheManager.caching({
-    store: redisStore,
+  const testClient = redis.createClient({
     db: config.cache.DB,
     port: config.cache.PORT,
   })
 
-  cache.store.events.on('redisError', error => {
-    console.log(error)
-    throw new Error(error)
-  })
+  if (testClient.ping()) {
+    const cache = cacheManager.caching({
+      store: redisStore,
+      db: config.cache.DB,
+      port: config.cache.PORT,
+    })
 
-  return cache
+    return cache
+  }
+
+  testClient.end(false)
+
+  // console.log('Redis cache unavailable. Using memory cache instead.')
+
+  // const cache = cacheManager.caching({
+  //   store: memoryStore,
+  // })
+
+  return {
+    set(...args) {
+      return null
+    },
+    get(...args) {
+      return null
+    },
+  }
 }
 
 module.exports = cacheConnectionFactory
