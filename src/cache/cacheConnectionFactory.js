d758bbcb8c0980ef1bbe5b270de06027c2b4c9bb
jgabriel1 <josegabriel@det.ufc.br>
2020-10-07 13:35:48 -0300

    Removed check for redis availability.

--- src/cache/cacheConnectionFactory.js
+++ src/cache/cacheConnectionFactory.js
@@ -1,36 +1,18 @@
 const cacheManager = require('cache-manager')
 const redisStore = require('cache-manager-redis')
 
-const redis = require('redis')
-
 const config = require('../config')
 
 function cacheConnectionFactory() {
-  const testClient = redis.createClient({
+  const store = redisStore.create()
+
+  const cache = cacheManager.caching({
+    store,
     db: config.cache.DB,
     port: config.cache.PORT,
   })
 
-  if (testClient.ping()) {
-    const cache = cacheManager.caching({
-      store: redisStore,
-      db: config.cache.DB,
-      port: config.cache.PORT,
-    })
-
-    console.log('Using Redis Cache.')
-
-    return cache
-  }
-
-  testClient.end(false)
-
-  console.log('Cache NOT enabled.')
-
-  return {
-    set: (...args) => null,
-    get: (...args) => null,
-  }
+  return cache
 }
 
 module.exports = cacheConnectionFactory
