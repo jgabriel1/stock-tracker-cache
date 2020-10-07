8b45ea7db249799c2d774eafddd7613570fdfc8a
jgabriel1 <josegabriel@det.ufc.br>
2020-10-07 16:16:50 -0300

    Added possibility of disabling cache for now.

--- src/cache/cacheConnectionFactory.js
+++ src/cache/cacheConnectionFactory.js
@@ -4,13 +4,27 @@ const redisStore = require('cache-manager-redis')
 const config = require('../config')
 
 function cacheConnectionFactory() {
+  const cacheEnabled = config.cache.USE_CACHE
+
+  if (!cacheEnabled) {
+    console.log('Cache is disabled.')
+
+    return {
+      get: (...args) => null,
+      set: (...args) => null,
+    }
+  }
+
   const store = redisStore.create()
 
   const cache = cacheManager.caching({
     store,
-    db: config.cache.DB,
-    port: config.cache.PORT,
+    url:
+      process.env.REDIS_URL ||
+      `redis://127.0.0.1:${config.cache.PORT}/${config.cache.DB}`,
   })
+
+  console.log('Cache is enabled.')
 
   return cache
 }
--- src/config/cache.js
+++ src/config/cache.js
@@ -2,4 +2,5 @@ module.exports = {
   PORT: 6379,
   DB: 0,
   TTL: 30,
+  USE_CACHE: process.env.USE_CACHE || false,
 }
