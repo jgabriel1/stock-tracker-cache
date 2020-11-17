3819c8648ed5511b4f2667dbe011c91ee2dd33cd
jgabriel1 <josegabriel@det.ufc.br>
2020-11-17 17:29:55 -0300

    Created route and controller for the chart data service.

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import ChartDataController from './controllers/ChartDataController';
 
 import PingController from './controllers/PingController';
 import StockInfoController from './controllers/StockInfoController';
@@ -9,10 +10,12 @@ const router = Router();
 const pingController = new PingController();
 const stockInfoController = new StockInfoController();
 const stockSearchController = new StockSearchController();
+const stockChartDataController = new ChartDataController();
 
 router.get('/', pingController.ping);
 
 router.get('/info', stockInfoController.index);
 router.get('/search', stockSearchController.index);
+router.get('/chart/:ticker', stockChartDataController.show);
 
 export default router;
